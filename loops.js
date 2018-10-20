33d7fe413f0ff8adf0adedab00e7b6c1cb001617
Terry Brown <ptbrown514@gmail.com>
2018-10-20 10:34:24 -0500

    initial commit

---
+++ loops.js
@@ -0,0 +1,11 @@
+var table = [
+    [ 'terry', 'taylor', 'scott', 'phil'],
+    [ 'sarah', 'rachel', 'lauren', 'joy'],
+    [ 'basketball', 'football', 'baseball', 'soccer'],
+    [ 'cbs', 'abc', 'fox', 'espn'],
+    ]
+
+for (i=0; i < table.length; i++) {
+    for (j=0; j< table[i].length; j++){
+    console.log(table[i][j]);
+}}
