144eda871d94015b1d682c34e97a4ad00b7e221f
Terry Brown <ptbrown514@gmail.com>
2018-10-20 12:33:27 -0500

    ignoring stuff

---
+++ index.js
@@ -0,0 +1,10 @@
+// Install package via npm Install
+// Load Package with require
+var _ = require('lodash');
+
+// Use package based on var we import with
+
+for(var i= 0; i < 100; i++){
+var r = _.random(10);
+
+console.log(r);}
