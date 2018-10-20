813ee44994411c731199be967a7779f60ff8bf3f
Terry Brown <ptbrown514@gmail.com>
2018-10-20 10:50:42 -0500

    making objects

--- loops.js
+++ loops.js
@@ -9,3 +9,9 @@ for (i=0; i < table.length; i++) {
     for (j=0; j< table[i].length; j++){
     console.log(table[i][j]);
 }}
+
+var personOne = {firstName: Terry, lastName: Brown, age: 29};
+var personTwo = {firstName: Nate, lastName: Rakestraw, age: 30};
+var personThree = {firstName: Sarah, lastName: Brown, age: 31};
+
+var friends = [personOne, personTwo, personThree];
