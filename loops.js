5aab0c1509d25a7b24aa71b0db25f2bdf618c8d5
Terry Brown <ptbrown514@gmail.com>
2018-10-20 11:49:38 -0500

    calling functions

--- loops.js
+++ loops.js
@@ -1,19 +1,49 @@
-var table = [
-    [ 'terry', 'taylor', 'scott', 'phil'],
-    [ 'sarah', 'rachel', 'lauren', 'joy'],
-    [ 'basketball', 'football', 'baseball', 'soccer'],
-    [ 'cbs', 'abc', 'fox', 'espn'],
-    ]
+// var table = [
+//     [ 'terry', 'taylor', 'scott', 'phil'],
+//     [ 'sarah', 'rachel', 'lauren', 'joy'],
+//     [ 'basketball', 'football', 'baseball', 'soccer'],
+//     [ 'cbs', 'abc', 'fox', 'espn'],
+//     ]
 
-for (i=0; i < table.length; i++) {
-    for (j=0; j< table[i].length; j++){
-    console.log(table[i][j]);
-}}
+// for (i=0; i < table.length; i++) {
+//     for (j=0; j< table[i].length; j++){
+//     console.log(table[i][j]);
+// }}
 
-var personOne = {firstname: "Terry", lastname: "Brown", age: 29};
-var personTwo = {firstname: "Nate", lastname: "Rakestraw", age: 30};
-var personThree = {firstname: "Sarah", lastname: "Brown", age: 31};
+// var personOne = {firstname: "Terry", lastname: "Brown", age: 29};
+// var personTwo = {firstname: "Nate", lastname: "Rakestraw", age: 30};
+// var personThree = {firstname: "Sarah", lastname: "Brown", age: 31};
 
-var friends = [personOne, personTwo, personThree];
+// var friends = [personOne, personTwo, personThree];
 
-console.log(friends.personOne.firstname);
+// var tmp = {
+//     repete3: function(x){
+//         for(var i = 1; i <= 3; i++){
+//             x();
+//         }
+//     }
+// }
+// function hello(){
+//     console.log('Hello');
+// }
+
+// tmp.repete3(hello);
+
+var math = {
+    add: function(one, two){
+        var sum = one + two;
+        console.log(sum);
+    },
+    subtract: function(one, two){
+        var diff = one - two;
+        console.log(diff)},
+    multiply: function (one, two){
+        var prod = one * two;
+        console.log(prod)},
+    divide: function(one, two){
+        var quot = one - two;
+        console.log(quot)}
+    }
+
+math.add(7, 8)
+math.subtract(9, 4)
